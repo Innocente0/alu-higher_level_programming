24b79add28b09cac78febb6c23a96d24526d48fd
Innocente0 <i.niwemwana@alustudent.com>
2024-02-28 14:52:58 -0800

    Switch

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # Complete the source code to switch the value of a and b
 a, b = b, a  # Insert this line to switch values of a and b
-print("a={} - b={}".format(a, b))
+print("a={}".format(a))  # This line prints the value of a
+print("b={}".format(b))  # This line prints the value of b
