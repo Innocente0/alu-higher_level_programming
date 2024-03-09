715beeef1e1f1975b189f4ad92657bad77c25e20
Innocente Niwemwana <i.niwemwana@alustudent.com>
2024-03-10 01:21:00 +0300

    4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
+""" A class that defines a square by its size
+    """
+
+
 class Square:
+    """ A class that defines a square by its size
+    """
+
     def __init__(self, size=0):
-        self.size = size
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
 
     @property
     def size(self):
+        """ Method to returns the size value
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Method to set the size value of the square object
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
-
-Square = __import__('4-square').Square
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+        else:
+            self.__size = value
