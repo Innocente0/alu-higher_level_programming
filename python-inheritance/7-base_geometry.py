e0e2c9e27750dfe52dd858fff65a8db7dd184fcb
Innocente Niwemwana <i.niwemwana@alustudent.com>
2024-03-17 15:29:05 +0200

    1-file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 7-base_geometry.py
 """Defines a base geometry class BaseGeometry."""
 
 
@@ -24,4 +23,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        
