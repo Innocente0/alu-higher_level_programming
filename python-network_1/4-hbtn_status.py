62846989088b7f881a860ed82e81b52120d8b2cf
Innocente0 <i.niwemwana@alustudent.com>
2024-03-24 13:40:55 -0700

    What's my status? #1

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
+import requests
+
+if _name_ == '_main_':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text)) no 4
